6a86693b9d376a796cfc99d103fcaf35dd35aa45
Julien  Broens <julienbroens@gmail.com>
2023-05-08 16:26:36 +0200

    add personnage component

---
+++ components/PersonnagePics.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const props = defineProps({
+  title: String,
+  imageUrl: String,
+  isOdd: Number,
+});
+</script>
+
+<template>
+  <div v-if="isOdd" class="relative">
+    <h3 class="absolute p-12 text-white text-6xl right-0 font-bold">
+      {{ title }}
+    </h3>
+    <img :alt="'photo de ' + title" :src="imageUrl" />
+  </div>
+  <div v-else class="relative">
+    <h3 class="absolute p-12 text-white text-6xl font-bold">{{ title }}</h3>
+    <img :alt="'photo de ' + title" :src="imageUrl" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/personnages/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const personnages = {
+  0: { title: "Denji", url: "/img/chainsaw-man-episode-7.png" },
+  1: { title: "Makima", url: "/img/Chainsaw-Man-Makima-2.webp" },
+};
+</script>
+
+<template>
+  <main>
+    <PersonnagePics
+      v-for="(personnage, index) in personnages"
+      :title="personnage.title"
+      :imageUrl="personnage.url"
+      :isOdd="index % 2"
+    />
+  </main>
+</template>
+
+<style scoped></style>
