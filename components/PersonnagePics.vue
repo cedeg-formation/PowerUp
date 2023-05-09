953a75da347d93129987bad50335428c8ed82b30
Julien  Broens <julienbroens@gmail.com>
2023-05-09 15:56:09 +0200

    buf fixed w-full

--- components/PersonnagePics.vue
+++ components/PersonnagePics.vue
@@ -11,11 +11,11 @@ const props = defineProps({
     <h3 class="absolute p-12 text-white text-6xl right-0 font-bold">
       {{ title }}
     </h3>
-    <img :alt="'photo de ' + title" :src="imageUrl" />
+    <img class="min-w-full" :alt="'photo de ' + title" :src="imageUrl" />
   </div>
   <div v-else class="relative">
     <h3 class="absolute p-12 text-white text-6xl font-bold">{{ title }}</h3>
-    <img :alt="'photo de ' + title" :src="imageUrl" />
+    <img class="min-w-full" :alt="'photo de ' + title" :src="imageUrl" />
   </div>
 </template>
 
