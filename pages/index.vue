c7f41d696678a35c3c111b37a9b9c30739cced30
Julien  Broens <julienbroens@gmail.com>
2023-05-16 10:53:52 +0200

    add header and buffer for plone

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,18 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Buffer } from "buffer";
+const username = "admin";
+const password = "admin";
+const { data: value } = await useFetch("http://0.0.0.0:8080/PowerUpAPI/about", {
+  headers: {
+    Accept: "application/json",
+    Authorization:
+      "Basic " + Buffer.from(username + ":" + password).toString("base64"),
+  },
+});
+
+//'Authorization', 'Basic ' + Buffer.from(username + ":" + password).toString('base64')
+console.log(value.value);
+</script>
 
 <template>
   <main>
